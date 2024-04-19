20cb913f302350bdcb796629a987b4df60a050be
dev-chpark <xpmxf5@gmail.com>
2024-04-19 18:08:18 +0900

    🔴 test: Red 테스트 내용 채움

--- src/test/java/org/wet/week1/FizzBuzzGameImplTest.java
+++ src/test/java/org/wet/week1/FizzBuzzGameImplTest.java
@@ -4,66 +4,91 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class FizzBuzzGameImplTest {
 
     @Test
     @DisplayName("3의 입력을 받은 피즈버즈 게임의 정답은 Fizz")
-    public void answerForThreeIsFizz() throws Exception {
+    void answerForThreeIsFizz() throws Exception {
         // given - 상황 만들기
+        int input = 3;
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGameImpl();
 
         // when - 동작
+        String answer = fizzBuzzGame.playGame(input);
 
         // then - 검증
+        assertEquals("Fizz", answer);
     }
 
     @Test
     @DisplayName("5의 입력을 받은 피즈버즈 게임의 정답은 Buzz")
-    public void answerForFiveIsBuzz() throws Exception {
+    void answerForFiveIsBuzz() throws Exception {
         // given - 상황 만들기
+        int input = 5;
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGameImpl();
 
         // when - 동작
+        String answer = fizzBuzzGame.playGame(input);
 
         // then - 검증
+        assertEquals("Buzz", answer);
     }
 
     @Test
     @DisplayName("15 가 입력된 피즈버즈 게임의 정답은 FizzBuzz")
-    public void answerForFifthteenIsFizzBuzz() throws Exception {
+    void answerForFifthteenIsFizzBuzz() throws Exception {
         // given - 상황 만들기
+        int input = 15;
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGameImpl();
 
         // when - 동작
+        String answer = fizzBuzzGame.playGame(input);
 
         // then - 검증
+        assertEquals("FizzBuzz", answer);
     }
 
     @Test
-    @DisplayName("3과 5의 배수가 아닌 숫자의 정답은 'fail'")
-    public void answerForNonThreeAndFiveMultipleFailsTheGame() throws Exception {
+    @DisplayName("3과 5의 배수가 아닌 자연수의 정답은 'Fail'")
+    void answerForNonThreeAndFiveMultipleFailsTheGame() throws Exception {
         // given - 상황 만들기
+        int input = 2;
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGameImpl();
 
         // when - 동작
+        String answer = fizzBuzzGame.playGame(input);
 
         // then - 검증
+        assertEquals("Fail", answer);
     }
 
     @Test
     @DisplayName("0의 피즈버즈 게임의 정답은 'Fail'")
-    public void answerForZeroFailsTheGame() throws Exception {
+    void answerForZeroFailsTheGame() throws Exception {
         // given - 상황 만들기
+        int input = 0;
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGameImpl();
 
         // when - 동작
+        String answer = fizzBuzzGame.playGame(input);
 
         // then - 검증
+        assertEquals("Fail", answer);
     }
 
     @Test
     @DisplayName("음수의 피즈버즈 게임의 정답은 'Fail'")
-    public void answerForNegativeFailsTheGame() throws Exception {
+    void answerForNegativeFailsTheGame() throws Exception {
         // given - 상황 만들기
+        int input = -1;
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGameImpl();
 
         // when - 동작
+        String answer = fizzBuzzGame.playGame(input);
 
         // then - 검증
+        assertEquals("Fail", answer);
     }
 
 }
