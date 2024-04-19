1c5a70b893953358e375eea219edde49c40b6e0e
dev-chpark <xpmxf5@gmail.com>
2024-04-19 18:16:46 +0900

    🟢 test: 정상 케이스 통과

--- src/main/java/org/wet/week1/FizzBuzzGameImpl.java
+++ src/main/java/org/wet/week1/FizzBuzzGameImpl.java
@@ -3,9 +3,16 @@ package org.wet.week1;
 public class FizzBuzzGameImpl implements FizzBuzzGame {
     @Override
     public String playGame(int number) {
-        if(number < 1)
+        if (number < 1)
             return "Fail";
 
-        return "fails";
+        StringBuilder sb = new StringBuilder();
+
+        if (number % 3 == 0) sb.append("Fizz");
+        if (number % 5 == 0) sb.append("Buzz");
+
+        if (sb.length() == 0) sb.append("Fail");
+
+        return sb.toString();
     }
 }
