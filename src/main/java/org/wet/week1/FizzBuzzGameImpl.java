adfe7d0653cc8d0a0b08492d6307a44e12c6552a
dev-chpark <xpmxf5@gmail.com>
2024-04-19 18:14:46 +0900

    🟢 test: 경계값 테스트 통과

--- src/main/java/org/wet/week1/FizzBuzzGameImpl.java
+++ src/main/java/org/wet/week1/FizzBuzzGameImpl.java
@@ -3,6 +3,8 @@ package org.wet.week1;
 public class FizzBuzzGameImpl implements FizzBuzzGame {
     @Override
     public String playGame(int number) {
+        if(number < 1)
+            return "Fail";
 
         return "fails";
     }
